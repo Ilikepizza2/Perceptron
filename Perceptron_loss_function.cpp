ef2fbb8c13d10bb36a6ec48ab113e89f8b3ecfbc
Ilikepizza2 <lit2021010@iiitl.ac.in>
2023-12-19 17:57:49 +0530

    added loss function

---
+++ Perceptron_loss_function.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<double> split_nums(string& line){
+    vector<double> vs;
+    string temp="";
+    for(int i=0;i<line.length();i++){
+        if(line[i]!=',')
+            temp.push_back(line[i]);
+        else{
+            vs.push_back(stod(temp));
+            temp="";
+        }
+    }
+    vs.push_back(stoi(temp)); // the last value
+    return vs;
+}
+
+void printCols(vector<vector<double>> inputs, vector<int> outputs){
+    int ind=0;
+    for(auto i: inputs){
+        for(auto j: i){
+            cout<<j<<',';
+        }
+        cout<<outputs[ind]<<'\n';
+        ind++;
+    }
+}
+
+void initializeRandomWeights(vector<double>& weights_and_bias) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> dis(-1.0, 1.0);  // Adjust the range based on your problem
+
+    for (double& weight : weights_and_bias) {
+        weight = dis(gen);
+    }
+}
+
+double sum(vector<double> v1, vector<double> v2){
+    v2.push_back(1); // bias term
+    double sum=0;
+    for(int i=0;i<v1.size();i++){
+        sum += v1[i]*v2[i];
+    }
+    return sum;
+}
+
+int classifier(vector<double> weights_and_bias, vector<double> input){
+    if(sum(weights_and_bias, input)>0)
+        return 1;
+    return 0;
+}
+
+int main(){
+    fstream fin, fout;
+    fin.open("train.csv", ios::in);
+    vector<vector<double>> inputs;
+    vector<int> outputs;
+    string temp;
+    fin>>temp; // eating up the first column(heading)
+    while(fin >> temp){
+        vector<double> splitted_nums = split_nums(temp);
+        inputs.push_back({});
+        for(int i=0;i<splitted_nums.size()-1;i++){
+            inputs.back().push_back(splitted_nums[i]);
+        }
+        outputs.push_back(splitted_nums.back());
+    }
+    // printCols(inputs, outputs);
+
+
+    vector<double> weights_and_bias(inputs.size()+1, 1);
+    initializeRandomWeights(weights_and_bias);
+    double learning_rate = 0.1;
+    int epochs = 1e3;
+
+    // training
+    while(epochs--){
+        // using loss function
+        for (int i = 0; i < inputs.size(); i++)
+        {
+            int predicted = classifier(weights_and_bias, inputs[i]);
+            if(predicted*outputs[i]<=0){
+                for(int j=0;j<inputs[i].size();j++){
+                    weights_and_bias[j] += learning_rate*outputs[i]*inputs[i][j];
+                }
+                weights_and_bias.back() += learning_rate*outputs[i];
+            }
+        }
+        
+    }
+
+
+
+    // testing
+    fin.close();
+    fin.open("test.csv", ios::in);
+    fin>>temp; // eating up the first column(heading)
+    while(fin >> temp){
+        vector<double> splitted_nums = split_nums(temp);
+        cout<<classifier(weights_and_bias, splitted_nums)<<'\n';
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Perceptron
-A neat, lightweight and single neuron perceptron written in C++ from scratch without any external library, trained using the perceptron trick. 
+# Perceptron Implementation in C++
+
+This C++ program demonstrates a basic implementation of a perceptron trained using the loss function and the perceptron trick. The perceptron is a fundamental building block of neural networks and is capable of binary classification.
+
+## Getting Started
+
+### Prerequisites
+
+- C++ Compiler
+
+### Usage
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/perceptron-cpp.git
+    ```
+
+2. Compile the C++ program:
+
+    ```bash
+    g++ perceptron.cpp -o perceptron
+    ```
+
+3. Run the executable:
+
+    ```bash
+    ./perceptron
+    ```
+
+### Input Data
+
+The program expects two CSV files:
+
+1. `train.csv` - Training data containing input features and corresponding binary labels.
+2. `test.csv` - Testing data for evaluating the trained perceptron.
+
+## Code Overview
+
+- `split_nums`: Function to split a string into a vector of doubles.
+- `printCols`: Function to print input features and labels.
+- `sum`: Function to calculate the sum of two vectors, including a bias term.
+- `classifier`: Function to classify input using the trained perceptron.
+
+The program performs training using the perceptron trick and then tests the perceptron on a separate dataset.
+
+## Parameters
+
+- Learning Rate: 0.1
+- Epochs: 1000
+
+## Considerations
+- This is a very basic single neuron perceptron with random weights. The sample data given is also very small (~100 rows). So the current outputs may vary `a lot`. Thus, it is for learning purposes only.
+
+## Contributing
+
+Feel free to contribute by opening issues or submitting pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgments
+
+- Inspired by the concept of perceptrons and neural networks.
